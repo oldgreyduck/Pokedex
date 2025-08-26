a3a97339b29ad9ca83cb9dea702a85cfefa30987
oldgreyduck <caquelin@hey.com>
2025-08-25 20:47:38 -0400

    CH1 L4 Complete

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+)
+
 
 func main() {
-        fmt.Println("Hello, World!")
+	scanner:= bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		inputLine := scanner.Text()
+		lowerInputLine := strings.ToLower(inputLine)
+		command := strings.Fields(lowerInputLine)
+		if len(command) > 0 {
+			fmt.Printf("Your command was: %s\n", command[0])
+		} else {
+			fmt.Printf("Your command was: (empty input)\n")
+		}
+	}
 }
