31122d6cec09f8a22617aea5430ed844d22a53ee
oldgreyduck <caquelin@hey.com>
2025-08-27 15:06:27 +0000

    Updated client.go and types_location.go

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -1,20 +1,43 @@
 package pokeapi
 
 import (
-    "net/http"
-    "time"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
 )
 
 type pokeapiClient struct {
-    httpClient    http.Client
-    cache         map[string][]byte
+	httpClient http.Client
+	cache      map[string][]byte
 }
 
-func NewpokeapiClient() *pokeapiClient {
-    return &pokeapiClient{
-        httpClient:  http.Client{
-	    Timeout: 10 * time.Second,
-},
-	cache: make(map[string][]byte),
+func NewPokeapiClient() *pokeapiClient {
+	return &pokeapiClient{
+		httpClient: http.Client{
+			Timeout: 10 * time.Second,
+		},
+		cache: make(map[string][]byte),
+	}
 }
+
+func (c *pokeapiClient) GetLocationAreas(locationURL string) (LocationAreaResp, error) {
+	resp, err := c.httpClient.Get(locationURL)
+	if err != nil {
+		return LocationAreaResp{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+i		return LocationAreaResp{}, err
+	}
+
+	var locationArea LocationAreaResp
+	err = json.Unmarshal(body, &locationArea)
+	if err != nil {
+		return LocationAreaResp{}, err
+	}
+
+	return locationArea, nil
 }
