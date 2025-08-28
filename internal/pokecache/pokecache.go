b6e1f992fd8619326a41fa30b4c16ed3b2db27fe
oldgreyduck <caquelin@hey.com>
2025-08-28 18:29:20 +0000

    still working CH2 L2

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -19,10 +19,12 @@ type cacheEntry struct {
 }
 
 func NewCache(interval time.Duration) *Cache {
-        return &Cache{
+        newCache :=  &Cache{
                 mapEntries:   make(map[string]cacheEntry)
                 interval:     interval
         }
+        go newCache.reapLoop(interval)
+        return newCache
 }
 
 func (c Cache) Add(key string, val []byte) {
@@ -34,4 +36,28 @@ func (c Cache) Add(key string, val []byte) {
 func (c Cache) Get(key string) []byte, bool {
         c.mu.Lock()
         defer c.mu.Unlock()
-        
+        val, ok := c.mapEntries[key]
+        if !ok {
+                return nil, false
+        }
+        return val.val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+        ticker := time.NewTicker(interval)
+        defer ticker.Stop()
+        for {
+                select {
+                case <- ticker.C:
+                        c.mu.Lock()
+                        defer c.mu.Unlock()
+                        now := time.Now()
+                        for key, entry := range c.mapEntries {
+                                if now.Sub(entry.createdAt) > interval {
+                                        delete(c.mapEntries, key)
+                                }
+                        }
+                }
+        }
+}
+
