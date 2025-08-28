cceebb559667c375029e1f460b3ebd61bfa5ba72
Christopher Caquelin <caquelin@hey.com>
2025-08-28 15:22:19 -0400

    Update repl_test.go

--- repl_test.go
+++ repl_test.go
@@ -7,85 +7,33 @@ func TestCleanInput(t *testing.T) {
         input    string
         expected []string
     }{
-        {
-            input:    "  hello  world  ",
-            expected: []string{"hello", "world"},
-        },
-	{
-	    input:    " charmander BULBAsaur PIKACHU   ",
-	    expected: []string{"charmander", "bulbasaur", "pikachu"},
-	},
-	{
-	    input:    "Magic The GATHERING",
-	    expected: []string{"magic", "the", "gathering"},
-	},
-    }
-
+    	{
+        	input:    "  hello  world  ",
+        	expected: []string{"hello", "world"},
+    	},
+		{
+	    	input:    " charmander BULBAsaur PIKACHU   ",
+	    	expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		{
+	    	input:    "Magic The GATHERING",
+	    	expected: []string{"magic", "the", "gathering"},
+		},
+	}
+		
     for _, c := range cases {
-	actual := cleanInput(c.input)
+		actual := cleanInput(c.input)
 	if len(actual) != len(c.expected) {
 		t.Errorf("For input '%s': expected %d, got %d", c.input, len(c.expected), len(actual))
     }
     for i := range actual {
-	word := actual[i]
-	expectedWord := c.expected[i]
-	if word != expectedWord {
-	    t.Errorf("For input '%s' at index %d: expected %v, got %v", c.input, i, expectedWord, word)
-    }
-}
-}
-}
-
-func TestAddGet(t *testing.T) {
-	const interval = 5 * time.Second
-	cases := []struct {
-		key string
-		val []byte
-	}{
-		{
-			key: "https://example.com",
-			val: []byte("testdata"),
-		},
-		{
-			key: "https://example.com/path",
-			val: []byte("moretestdata"),
-		},
-	}
-
-	for i, c := range cases {
-		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
-			cache := NewCache(interval)
-			cache.Add(c.key, c.val)
-			val, ok := cache.Get(c.key)
-			if !ok {
-				t.Errorf("expected to find key")
-				return
-			}
-			if string(val) != string(c.val) {
-				t.Errorf("expected to find value")
-				return
-			}
-		})
+		word := actual[i]
+		expectedWord := c.expected[i]
+		if word != expectedWord {
+	    	t.Errorf("For input '%s' at index %d: expected %v, got %v", c.input, i, expectedWord, word)
+    	}
 	}
 }
+}
 
-func TestReapLoop(t *testing.T) {
-	const baseTime = 5 * time.Millisecond
-	const waitTime = baseTime + 5*time.Millisecond
-	cache := NewCache(baseTime)
-	cache.Add("https://example.com", []byte("testdata"))
-
-	_, ok := cache.Get("https://example.com")
-	if !ok {
-		t.Errorf("expected to find key")
-		return
-	}
-
-	time.Sleep(waitTime)
-
-	_, ok = cache.Get("https://example.com")
-	if ok {
-		t.Errorf("expected to not find key")
-		return
-	}
 }
